899b7a5bc42c839f690e07e9d964257625e79acb
Viktor dias <61331026+viktordias@users.noreply.github.com>
2023-08-09 17:38:41 -0300

    tcc

---
+++ programa/classes.php
@@ -0,0 +1,36 @@
+<?php
+class Dados{ 
+    public $id;
+    public $valor;
+
+}   
+
+class minemonico{
+    public $NOP = 00;
+    public $STA = 16;
+    public $LDA = 32;
+    public $ADD = 48;
+    public $OR = 64;
+    public $AND = 80;
+    public $NOT = 96;
+    public $JMP = 128;
+    public $JN = 144;
+    public $JZ = 160;
+    public $HLT = 240;
+}
+
+class programa{
+    public $id;
+    public $dado;
+    public $minemonico;
+    
+}
+
+class visor{
+    public $PC;
+    public $AC;
+    public $acessos;
+    public $instrucoes;
+}
+
+?>
---
+++ programa/main.js
@@ -0,0 +1,44 @@
+//https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css
+//https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js
+//https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
+//https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+   
+    const showNavbar = (toggleId, navId, bodyId, headerId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId),
+    bodypd = document.getElementById(bodyId),
+    headerpd = document.getElementById(headerId)
+    
+    // Validate that all variables exist
+    if(toggle && nav && bodypd && headerpd){
+    toggle.addEventListener('click', ()=>{
+    // show navbar
+    nav.classList.toggle('show')
+    // change icon
+    toggle.classList.toggle('bx-x')
+    // add padding to body
+    bodypd.classList.toggle('body-pd')
+    // add padding to header
+    headerpd.classList.toggle('body-pd')
+    })
+    }
+    }
+    
+    showNavbar('header-toggle','nav-bar','body-pd','header')
+    
+    /*===== LINK ACTIVE =====*/
+    const linkColor = document.querySelectorAll('.nav_link')
+    
+    function colorLink(){
+    if(linkColor){
+    linkColor.forEach(l=> l.classList.remove('active'))
+    this.classList.add('active')
+    }
+    }
+    linkColor.forEach(l=> l.addEventListener('click', colorLink))
+    
+     // Your code to run since DOM is loaded and ready
+    });
