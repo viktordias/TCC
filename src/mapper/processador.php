f225b57e24407116033308820c86f4b38bcf9516
Viktor dias <61331026+viktordias@users.noreply.github.com>
2023-08-17 23:48:13 -0300

    cc

---
+++ src/mapper/processador.php
@@ -0,0 +1,24 @@
+<?php
+class Registrador{ 
+    public $id;
+    public $valor;
+
+}   
+
+class instrucoes{
+    public $id;
+    public $minemonico;
+}
+
+class Ap{
+    public $PC;
+   
+}
+
+class Ac{
+    public $AC;
+    public $acessos;
+    public $Contador;
+}
+
+?>
